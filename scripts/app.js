3ce5cbc9d78bc7dbd6238f6e632ab47ba72f6855
Jon Bascos <jon@jonbascos.com>
2021-10-29 11:23:22 -0700

    added popup modals after clicking on profile pictures. Renamed images.

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,57 @@
-console.log('Running...')
-
 // Countdown Timer 
 
-let endDate = new Date("Nov 03, 2021 10:00:00").getTime()
+const endDate = new Date("Nov 03, 2021 10:00:00").getTime()
 
-let timer = setInterval(() => {
-    let now = new Date().getTime()
-    let end = endDate - now
+const timer = setInterval(() => {
+    const now = new Date().getTime()
+    const end = endDate - now
 
     if(end >= 0) {
-        let days = Math.floor(end / (1000 * 60 * 60 * 24))
-    let hours = Math.floor((end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) 
-    let mins = Math.floor((end % (1000 * 60 * 60)) / (1000 * 60))
-    let secs = Math.floor((end % (1000 * 60)) / 1000)
+        const days = Math.floor(end / (1000 * 60 * 60 * 24))
+        const hours = Math.floor((end % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) 
+        const mins = Math.floor((end % (1000 * 60 * 60)) / (1000 * 60))
+        const secs = Math.floor((end % (1000 * 60)) / 1000)
 
-    document.querySelector('.timer-days').innerHTML = days
-    document.querySelector('.timer-hours').innerHTML = hours
-    document.querySelector('.timer-minutes').innerHTML = mins
-    document.querySelector('.timer-seconds').innerHTML = secs
+        document.querySelector('#timerDays').innerHTML = days
+        document.querySelector('#timerHours').innerHTML = hours
+        document.querySelector('#timerMinutes').innerHTML = mins
+        document.querySelector('#timerSeconds').innerHTML = secs
+        }
+    }, 1000)
+
+/* Modal Popup */
+
+// Grabs all modals
+
+const modalBtns = document.querySelectorAll('.modal-open')
+
+// Displays the modal for that profilePicture
+modalBtns.forEach(btn => {
+    btn.onclick = () => {
+        const modal = btn.getAttribute("data-modal")
+        document.getElementById(modal).style.display = "block"
     }
+})
 
-    
-}, 1000)
+// Closes the modal when you click on the "close" button
+const closeBtns = document.querySelectorAll('.modal-close')
+
+closeBtns.forEach(btn => {
+    btn.onclick = () => {
+        const modal = btn.closest(".modal").style.display = 'none'
+    }
+})
+
+// Closes modal when clicking anywhere on the screen on a desktop/laptop 
+window.onclick = e => {
+    if(e.target.className === 'modal') {
+        e.target.style.display = 'none'
+    } 
+}
+
+// Closes modal when tapping anywhere on the screen that isn't another modal on a mobile device.
+window.ontouchstart = e => {
+    if(e.target.className === 'modal') {
+        e.target.style.display = 'none'
+    } 
+}
